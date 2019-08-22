fc3acea85b7cb0e79ef624bb9b257d1d6f5aa6e5
Ben Kile <benjaminfkile@gmail.com>
2019-08-22 00:01:39 -0600

    logos working and css styling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var stream = "http://stream.dedyn.io:8000/dsbmradio.opus";
-var limit = 500;
+var limit = 50;
 var proxyUrl = "https://cors-anywhere.herokuapp.com/"
 
 function parseUrl(args) {
@@ -18,14 +18,12 @@ function parseUrl(args) {
             console.log(url);
             //stream = url;
             renderPlayer(url);
-
         }
     };
     x.send(null);
-    //console.log(`station id: ${args}`);
-    //console.log(proxyUrl + targetUrl + args);
+    console.log(`station id: ${args}`);
+    console.log(proxyUrl + targetUrl + args);
 }
-
 function renderPlayer(args) {
     $(".player").empty();
     $(".player").append(`  <audio controls autoplay>
@@ -35,7 +33,6 @@ Your browser does not support the audio element.
 `);
 }
 function queryGenre(args) {
-
     var targetUrl = `http://api.shoutcast.com/station/advancedsearch?mt=audio/mpeg&search=${args}&limit=${limit}&f=json&k=OVxbFpTaTgaBkwGC`;
     fetch(proxyUrl + targetUrl)
         .then(response => response.json())
@@ -44,12 +41,16 @@ function queryGenre(args) {
     //console.log(q);  
 }
 function renderStations(args) {
-    $(".stations ul").empty();
+    $(".stations").empty();
     for (let i = 0; i < args.response.data.stationlist.station.length; i++) {
-        $(".stations ul").append(`<li><div class="${args.response.data.stationlist.station[i].id}" onclick="parseUrl(this.className)">${args.response.data.stationlist.station[i].name}`);
-        //$(".stations").append(`<h3>${args.response.data.stationlist.station[i].name}</h3>`);
-        $(".stations ul").append(`</div></li>`);
-
+        $(".stations").append(
+            `<div class="${args.response.data.stationlist.station[i].id}" onclick="parseUrl(this.className)">
+            <h1>${args.response.data.stationlist.station[i].name}</h1>
+            <object data="${args.response.data.stationlist.station[i].logo}" type="image/png">
+            <img src="headphones.png" alt="Stack Overflow logo and icons and such">
+          </object>
+            <ul><li>${args.response.data.stationlist.station[i].ct}</li></ul>
+            <div/>`);
     }
 }
 function getGenres() {
@@ -62,27 +63,24 @@ function getGenres() {
 function renderGenres(args) {
     for (let i = 0; i < args.response.data.genrelist.genre.length; i++) {
         //console.log(args.response.data.genrelist.genre[i].name);
-       $("#myDropdown").append(`<li><div class="${args.response.data.genrelist.genre[i].name}" onclick=queryGenre(this.className),drop()>${args.response.data.genrelist.genre[i].name}`);
-       $("#myDropdown").append(`</div></li>`);
-       
+        $("#genreSearch").append(`<li><div class="${args.response.data.genrelist.genre[i].name}" onclick=queryGenre(this.className),drop()>${args.response.data.genrelist.genre[i].name}`);
+        $("#genreSearch").append(`</div></li>`);
+
     }
-    
 }
 /*************************css stuff***********************************/
 function drop() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("genreSearch").classList.toggle("show");
 }
 
 function burger(x) {
     x.classList.toggle("change");
 }
-
-
 function filterFunction() {
     var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
+    input = document.getElementById("input");
     filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
+    div = document.getElementById("genreSearch");
     a = div.getElementsByTagName("li");
     for (i = 0; i < a.length; i++) {
         txtValue = a[i].textContent || a[i].innerText;
@@ -93,7 +91,6 @@ function filterFunction() {
         }
     }
 }
-/**********************************************************************/
+queryGenre("alternative")
 getGenres();
-queryGenre("hiphop");
 
