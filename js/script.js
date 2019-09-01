715cc9a1cf61fd92ce19bd6871a81afe47b2ac2a
Ben Kile <benjaminfkile@gmail.com>
2019-09-01 00:01:18 -0600

    new test, pushed wrong js file

--- js/script.js
+++ js/script.js
@@ -1,116 +1,113 @@
 //the queu
-var queu = [];
+let queue = [];
 //heroku proxy
-var proxyUrl = "https://cors-anywhere.herokuapp.com/"
+let proxyUrl = "https://cors-anywhere.herokuapp.com/";
 //my dev key
-var key = "OVxbFpTaTgaBkwGC"
+let key = "OVxbFpTaTgaBkwGC";
 //limit of results returned
-var limit = 25;
+let limit = 50;
 //called first, get search results from shoutcast
 function queryGenre(genre, limit) {
     if ((genre !== "Search...") && (genre !== "")) {
-        var targetUrl = `http://api.shoutcast.com/station/advancedsearch?mt=audio/mpeg&search=${genre}&limit=${limit}&f=json&k=${key}`;
+        let targetUrl = `http://api.shoutcast.com/station/advancedsearch?mt=audio/mpeg&search=${genre}&limit=${limit}&f=json&k=${key}`;
         fetch(proxyUrl + targetUrl)
             .then(response => response.json())
-            .then(responseJson => buildQueu(responseJson))
+            .then(responseJson => buildQueue(responseJson))
             .catch(error => alert("NOTHING FOUND, TRY AGAIN"));
     } else {
         alert("SEARCH FOR A GENRE BELOW!");
-    }
-}
-//called second, builds an array of station objects
-function buildQueu(args) {
-    queu = [];
-    var response = args.response.data.stationlist.station;
-    for (let i = 0; i < response.length; i++) {
-        queu.push({
-            name: response[i].name,
-            genre: response[i].genre,
-            currentTrack: response[i].ct,
-            bitRate: response[i].br,
-            listeners: response[i].lc,
-            logo: response[i].logo,
-            id: response[i].id
-        });
-    }
-    renderStations(queu);
-}
-//called third, renders dom elements and listeners and empties old elements
-function renderStations(args) {
-    $(".searchResults").empty();
-    for (let i = 0; i < args.length; i++) {
-        $(".searchResults").append(
-            `<div class="${args[i].id}" onclick="parseUrl(this.className)">
-                <h1>${args[i].name}
-                </h1>
-                <img src="${args[i].logo}" alt="Not Found" onerror=this.src="headphones.png">
-                <ul>
-                <li>Genre: ${args[i].genre}</li>
-                <li>${args[i].bitRate} bps</li>
-                <li>Listeners: ${args[i].listeners}</li>
-                <li>Now Playing: ${args[i].currentTrack}</li>
-                </ul>
-                </div>`);
-    }
-}
-function parseUrl(args) {
-    var targetUrl = `http://yp.shoutcast.com/sbin/tunein-station.xspf?id=`;
+    };
+};
 
-    fetch(proxyUrl + targetUrl + args, { mode: 'cors' })
-        .then(response => response.text())
-        .then(responseXML => {
-            // console.log(responseXML);
-            let oParser = new DOMParser();
-            let oDOM = oParser.parseFromString(responseXML, "application/xml");
-            // console.log(oDOM);
-            let url;
-            try {
-                url = oDOM.getElementsByTagName("location")[0].textContent
-            } catch{
-                alert("STATION UNAVAILABLE");
-                $(`.${args}`).remove();
-            }
-            renderPlayer(url, args);
-        });
+function buildQueue(args) {
+    var targetUrl = `http://yp.shoutcast.com/sbin/tunein-station.xspf?id=`;
+    var response = args.response.data.stationlist.station;
+    queue = [];
+
+    $(".searchResults").empty();
+    for (let i = 0; i < response.length; i++) {
+        fetch(proxyUrl + targetUrl + response[i].id, { mode: 'cors' })
+            .then((res) => res.text())
+            .then(responseXML => {
+                //console.log(responseXML);
+                let oParser = new DOMParser();
+                let oDOM = oParser.parseFromString(responseXML, "application/xml");
+                try {
+                    let station = {
+                        name: response[i].name,
+                        genre: response[i].genre,
+                        currentTrack: response[i].ct,
+                        bitRate: response[i].br,
+                        listeners: response[i].lc,
+                        logo: response[i].logo,
+                        id: response[i].id,
+                        url: oDOM.getElementsByTagName("location")[0].textContent
+                    };
+                    queue.push(station);
+                    //renderStation(station)
+                    checkAudio(station);
+
+                } catch{
+                    console.log("INVALID RESPONSE");
+                }
+            });
+
+    }
 }
-function renderPlayer(url, id) {
-    renderStations(queu);
-    $(`.${id}`).append(`
-            <audio id="aud" controls="controls">
-            <source src=${url} type="audio/mpeg">
-            </audio>`);
+function checkAudio(station) {
+    $(".audioTest").empty();
+    $(".audioTest").append(`
+    <audio id="aud" controls="controls">
+    <source src=${station.url} type="audio/mpeg">
+    </audio>`);
     const audio = document.querySelector('audio');
-    
     audio.oncanplay = (event) => {
         console.log("success");
-        success(id);
+        renderStation(station);
     };
-    audio.onerror = (event) => {
-        console.log("error");
-    };
-    audio.onabort = (event) => {
-        console.log("abort");
-    };
-    audio.onstalled = (event) => {
-        console.log("stalled");
-    };
-    audio.onsuspend = (event) => {
-        console.log("suspend");
-    };
-}
-function success(args) {
-    $("audio").trigger("play");
-    $(".load").empty();
 
 }
-//function that searches genres
+function renderStation(station) {
+    //console.log(station.url);
+    $(".searchResults").append(
+        `<div class="${station.id}" id="${station.id}">
+                <h1>${station.name}
+                </h1>
+                <img src="${station.logo}" alt="Not Found" onerror=this.src="headphones.png">
+                <ul>
+                <li>Genre: ${station.genre}</li>
+                <li>${station.bitRate} bps</li>
+                <li>Listeners: ${station.listeners}</li>
+                <li>Now Playing: ${station.currentTrack}</li>
+                </ul>
+                </div>`);
+    let id = station.id;
+    let url = station.url;
+    $(`#${station.id}`).click(function () {
+        play(id, url)
+    });
+}
+function play(id, url) {
+    //console.log(id);
+    //console.log(url);
+    $(".nowPlaying").remove();
+    $(`.${id}`).append(`
+        <div class="nowPlaying">
+        <audio controls="controls" autoplay>
+        <source src=${url} type="audio/mpeg">
+        </audio>
+        </div>`
+    );
+}
+// queryGenre("edm", lim);
+
 function searchGenre() {
     //scroll back to top on new search
     $(".searchResults").animate({ scrollTop: 0 }, 1000);
-    var search = (document.getElementById("searchInput").value);
+    let search = (document.getElementById("searchInput").value);
     queryGenre(search, limit)
 }
-//wait to add listeners (do i need to wait?)
+
 $(document).ready(function () {
     $("#searchBtn").click(function () { searchGenre() });
     $("input").keypress(function (e) {
