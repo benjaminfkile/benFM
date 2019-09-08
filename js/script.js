63fd88bf748736377d3b2a41635dcc692a370061
Ben Kile <benjaminfkile@gmail.com>
2019-09-07 22:07:20 -0600

    think everything is working now

--- js/script.js
+++ js/script.js
@@ -1,40 +1,36 @@
 //initialize an empty station array
 let queue = [];
 //heroku proxy URL to stop corb errors
-let proxyUrl = "https://cors-anywhere.herokuapp.com/";
+let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 //my shoutcast developer key
-let key = "OVxbFpTaTgaBkwGC";
+let key = 'OVxbFpTaTgaBkwGC';
 //limit of results from shoutcast
-let limit = 25;
-var eventIndex = -1;
-/*
-query ShoutCast with params passed from 
-searchGenre(genre,limit)
-*/
+let limit = 50;
+//makes a call to the shoutcast api 
 function queryGenre(genre, limit) {
-    $("#hS h4").css("color", "rgb(255,255,255)");
-    if ((genre !== "Search...") && (genre !== "")) {
+    if ((genre !== 'Search...') && (genre !== '')) {
         let targetUrl = `http://api.shoutcast.com/station/advancedsearch?mt=audio/mpeg&search=${genre}&limit=${limit}&f=json&k=${key}`;
         fetch(proxyUrl + targetUrl)
             .then(response => response.json())
             .then(responseJson => buildQueue(responseJson))
-            .catch(error => alert("NOTHING FOUND, TRY AGAIN"));
+            .catch(error => alert('NOTHING FOUND, TRY AGAIN'));
     } else {
-        alert("SEARCH FOR A GENRE BELOW!");
+        alert('SEARCH FOR A GENRE BELOW!');
     };
 };
-//build an array with station objects from the response
+//creates an array of station objects
+//and calls checkAudio(station) in the loop
 function buildQueue(args) {
     let targetUrl = `http://yp.shoutcast.com/sbin/tunein-station.xspf?id=`;
     let response = args.response.data.stationlist.station;
     queue = [];
-    $(".searchResults").empty();
+    $('.searchResults').empty();
     for (let i = 0; i < response.length; i++) {
         fetch(proxyUrl + targetUrl + response[i].id, { mode: 'cors' })
             .then((res) => res.text())
             .then(responseXML => {
                 let oParser = new DOMParser();
-                let oDOM = oParser.parseFromString(responseXML, "application/xml");
+                let oDOM = oParser.parseFromString(responseXML, 'application/xml');
                 try {
                     let station = {
                         name: response[i].name,
@@ -44,110 +40,114 @@ function buildQueue(args) {
                         listeners: response[i].lc,
                         logo: response[i].logo,
                         id: response[i].id,
-                        url: oDOM.getElementsByTagName("location")[0].textContent,
+                        url: oDOM.getElementsByTagName('location')[0].textContent,
                         state: 1
                     };
                     queue.push(station);
                     checkAudio(station);
                 } catch{
-                    console.log("INVALID RESPONSE");
+                    //console.log('INVALID RESPONSE');
                 }
             });
     }
 }
-/*
-if the stream can play, call renderStation(station) and 
-render the the new <div></div> to the DOM
-*/
+//first render the station 
+//if it wont play then hide it
 function checkAudio(station) {
+    renderStation(station);
     let audioElement = document.createElement('audio');
     audioElement.src = station.url;
-    audioElement.oncanplay = function () {
-        renderStation(station);
+    audioElement.onerror = function () {
+        $(`.${station.id}`).hide();
     }
+
 }
-//apend the html elements to the DOM call by call
+//render the stations to the DOM
 function renderStation(station) {
-    $(".searchResults").append(`
-                    <div class="${station.id}" id="${station.id}">
+    $('.searchResults').append(`
+                    <div class='${station.id}' id='${station.id}'>
                     <h1>${station.name}
                     </h1>
-                    <img src="${station.logo}" alt="Not Found" onerror=this.src="headphones.png">
+                    <img src=${station.logo} alt='Not Found' onerror=this.src='./res/headphones.png'>
                     <ul>
                     <li>Genre: ${station.genre}</li>
-                    <li>Bitrate: ${station.bitRate}</li>
+                    <li>${station.bitRate} bps</li>
                     <li>Listeners: ${station.listeners}</li>
-                    <li>Now Playing: ${station.currentTrack}</li>
                     </ul>
+                    <div class='stationState' id='${station.id}state'>
+                    </div>
                     </div>`
     );
+    //add an click listener to each station 
     $(`#${station.id}`).click(function () {
         station.state++;
         shout(station.id, station.url, station.state)
     });
 }
-//finally play the stream on user click
+//clear all other audio
+//play the station and set up loading bars
 function shout(id, url, state) {
-    $(".searchResults div").css("background-color", "rgb(255,255,255)");
-    var sounds = document.getElementsByTagName('audio');
-    for (i = 0; i < sounds.length; i++) sounds[i].pause();
+    let sounds = document.getElementsByTagName('audio');
+    for (i = 0; i < sounds.length; i++) sounds[i].remove();
+    $('.stationState').empty();
     if (state % 2 == 0) {
-        eventIndex++;
-        $(`.${id}`).remove("audio");
         $(`.${id}`).append(`
-            <audio id="aud" controls autoplay>
-            <source src=${url} type="audio/mpeg">
+            <audio id='aud' controls autoplay>
+            <source src=${url} type='audio/mpeg'>
             </audio>`
         );
-        $(".searchResults div").css("border","none");
-        $(`.${id}`).css("border","15px solid white");
+        $('audio').hide();
+        sounds = document.getElementsByTagName('audio')[0];
+        sounds.addEventListener('playing', function () {
+            console.log('play');
+            $('.stationState').empty();
+            $(`#${id}state`).append(`
+            <div class='lds-facebook'>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            </div>`);
+        }, true)
 
-        $("audio").hide();
+        sounds.addEventListener('loadstart', function () {
+            console.log('load');
+            $('.state').empty();
+            $(`#${id}state`).append(`
+            <div class='lds-ellipsis'>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            </div>`);
+        }, true)
     }
-    var stream = document.getElementsByTagName("audio")[eventIndex];
-    stream.addEventListener("pause", function () {
-        console.log("pause");
-        $(".lds-ripple").hide();
-        $(".lds-facebook").hide();
-    }, true);
-    stream.addEventListener("play", function () {
-        console.log("play");
-        $(".lds-ripple").hide();
-        $(".lds-facebook").show();
-    }, true);
-    stream.addEventListener("loadstart", function () {
-        console.log("load");
-        $(".lds-ripple").show();
-        $(".lds-facebook").hide();
-    }, true);
+    else {
+       sounds = document.getElementsByTagName('audio');
+       for (i = 0; i < sounds.length; i++) sounds[i].remove();
+        console.log('stop');
+    }
 }
-
-/*
-scroll the contents of the .searchResults div in the html file
-back to the top and get the new search results from the DOM
-call queryGenre with params "search" and "limit"
-*/
+//search a genre
 function searchGenre() {
-    eventIndex = -1;
-
-    $(".searchResults").animate({ scrollTop: 0 }, 1000);
-    let search = (document.getElementById("searchInput").value);
+    $('.searchResults').empty();
+    let search = (document.getElementById('searchInput').value);
+    $('.searchResults').append(`
+    <h5>Searching: '${search}'<h5>`);
     queryGenre(search, limit)
-
 }
-/*
-add search button listener and key #13 listener then
-call searchGenre() on #13 keypress or button click
-*/
+//set up click and return key listener for input
 $(document).ready(function () {
-    $("#searchBtn").click(function () { searchGenre() });
-    $("input").keypress(function (args) {
+    $('#searchBtn').click(function () { searchGenre() });
+    $('input').keypress(function (args) {
         if (args.which == 13) {
             searchGenre();
         }
     });
-    $(".lds-ripple").hide();
-    $(".lds-facebook").hide();
 });
-
-
