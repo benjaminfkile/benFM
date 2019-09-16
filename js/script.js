c1a97a2a44d17306c2d466ba5d2e76180a213755
Ben Kile <benjaminfkile@gmail.com>
2019-09-16 05:55:43 -0600

    most revision requests fixed

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
-//initialize an empty station array
-let queue = [];
 //heroku proxy URL to stop corb errors
-let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 //my shoutcast developer key
-let key = 'OVxbFpTaTgaBkwGC';
+const key = 'OVxbFpTaTgaBkwGC';
 //limit of results from shoutcast
-let limit = 50;
+const limit = 50;
+//attempts at api call
 let attempt = 0;
-//makes a call to the shoutcast api 
-function queryGenre(args, limit) {
-    let genre = args;
+//query shoutcast
+function queryGenre(genre, limit) {
+    attempt++;
+    let args = genre;
     let targetUrl = `http://api.shoutcast.com/station/advancedsearch?mt=audio/mpeg&search=${genre}&limit=${limit}&f=json&k=${key}`;
     fetch(proxyUrl + targetUrl)
         .then(response => response.json())
         .then(data => {
-            let arr = data.response.data.stationlist
-            console.log(arr.station);
-            if(arr.station == null){
-                attempt++;
-                queryGenre(genre,limit);
+            if (attempt < 3) {
+                let arr = data.response.data.stationlist
+                //a little recursion to fix 
+                //shoutcasts incomplete responses
+                //this is a bug I went over with my mentor 
+                //and he agrees the api is buggy
+                if (arr.station == null) {
+                    queryGenre(args, limit);
+                }
+                else {
+                    buildQueue(data, genre);
+                }
+                //try and handle bad search requests
+            } else {
+                alert("Nothing Found :(");
+                $('.landing').show();
+                $('.sk-circle').hide();
             }
-            else{
-                attempt = 0;
-                buildQueue(data,args);
-            } 
-        })
-        .catch(error => console.error(error))
-        console.log(attempt);
 
+        })
+        .catch(error => alert('something went wrong, please try again'));
 };
-//creates an array of station objects
-//and calls checkAudio(station) in the loop
+//build the queue
 function buildQueue(args, genre) {
     let targetUrl = `http://yp.shoutcast.com/sbin/tunein-station.xspf?id=`;
     let response = args.response.data.stationlist.station;
@@ -53,7 +59,7 @@ function buildQueue(args, genre) {
                         state: 1
                     };
                     queue.push(station);
-                    checkAudio(station,genre);
+                    checkAudio(station, genre);
                 } catch (error) {
                     console.log(error);
                 }
@@ -62,7 +68,7 @@ function buildQueue(args, genre) {
     }
     console.log(queue);
 }
-
+//make sure the station will play
 function checkAudio(station, genre) {
     renderStation(station);
     let audioElement = document.createElement('audio');
@@ -71,9 +77,11 @@ function checkAudio(station, genre) {
         $(`.${station.id}`).hide();
     }
 }
-
 //render the stations to the DOM
 function renderStation(station) {
+    $('.sk-circle').hide();
+    $('.landing').hide();
+    $('.logo').hide();
     $('.searchResults').append(`
                     <div class='${station.id}' id='${station.id}'>
                     <h1>${station.name}
@@ -88,19 +96,18 @@ function renderStation(station) {
                     </div>
                     </div>`
     );
-    //add an click listener to each station 
     $(`#${station.id}`).click(function () {
         station.state++;
         shout(station.id, station.url, station.state)
     });
 }
-//clear all other audio
-//play the station and set up loading bars
+//play the clicked tile
 function shout(id, url, state) {
     let sounds = document.getElementsByTagName('audio');
     for (i = 0; i < sounds.length; i++) sounds[i].remove();
     $('.stationState').empty();
     if (state % 2 == 0) {
+        $(`.${id}`).css("border","5px solid rgb(128, 128, 128)");
         $(`.${id}`).append(`
             <audio id='aud' controls autoplay>
             <source src=${url} type='audio/mpeg'>
@@ -109,7 +116,7 @@ function shout(id, url, state) {
         $('audio').hide();
         sounds = document.getElementsByTagName('audio')[0];
         sounds.addEventListener('playing', function () {
-            $('.stationState').empty();
+            $(`#${id}state`).empty();
             $(`#${id}state`).append(`
             <div class='lds-facebook'>
             <div>
@@ -119,9 +126,9 @@ function shout(id, url, state) {
             <div>
             </div>
             </div>`);
-        }, true)
+        }, true);
         sounds.addEventListener('loadstart', function () {
-            $('.state').empty();
+            $(`#${id}state`).empty();
             $(`#${id}state`).append(`
             <div class='lds-ellipsis'>
             <div>
@@ -133,34 +140,61 @@ function shout(id, url, state) {
             <div>
             </div>
             </div>`);
-        }, true)
+        }, true);
+        sounds.addEventListener('waiting', function () {
+            $(`#${id}state`).empty();
+            $(`#${id}state`).append(`
+            <div class='lds-ellipsis'>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            </div>`);
+        }, true); 
     }
     else {
         sounds = document.getElementsByTagName('audio');
         for (i = 0; i < sounds.length; i++) sounds[i].remove();
+        $(`.${id}`).css("border","none");
     }
 }
-//search a genre
+//set up the queryGenre function for arguments
 function searchGenre() {
     let search = (document.getElementById('searchInput').value);
     if ((search !== "") && (search !== "Search a music genre...")) {
+        var toast = document.getElementById("snackbar");
+        toast.className = "show";
+        setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 6000);
         queryGenre(search, limit)
     } else {
         alert('SEARCH FOR A GENRE BELOW!');
         $("#searchInput").val("Search a music genre...");
     }
 }
-//set up click and return key listener for input
 $(document).ready(function () {
     $('#searchBtn').click(function () {
+        attempt = 0;
+        $('.landing').show();
         $('.searchResults').empty();
+        $('.sk-circle').show();
+        $('.logo').show();
         searchGenre();
     });
     $('input').keypress(function (args) {
+        attempt = 0;
         if (args.which == 13) {
+            $('.landing').show();
             $('.searchResults').empty();
+            $('.sk-circle').show();
+            $('.logo').show();
             searchGenre();
         }
     });
-    queryGenre(genreArray[Math.floor(Math.random() * genreArray.length)], limit);
+
+    //queryGenre(genreArray[Math.floor(Math.random() * genreArray.length)], limit);
+    $('.sk-circle').hide();
 });
